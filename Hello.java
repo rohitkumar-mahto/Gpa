c1f5f9af46aefa25a8aab0eea53fa0a1e60da85d
rohitkumar-mahto <rohit.mahto9512@gmail.com>
2026-04-23 08:03:22 +0530

    update successfully

--- Hello.java
+++ Hello.java
@@ -2,9 +2,11 @@ import java.util.*;
 class Hello{
   public static void main(String args[]){
     int a ;
-    Scanner sc = new Scanner(System.in);
+    try(Scanner sc = new Scanner(System.in)){
     System.out.println("Enter the number:");
     a = sc.nextInt();
     System.out.println("a=" + a);
+    sc.close();
+    }
   }
 }
